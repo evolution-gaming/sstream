e14b8ba5b2a4ab9dd11c999b9cc36af2b35d453c
mr-git <8796159+mr-git@users.noreply.github.com>
2019-09-12 09:50:09 +0300

    use Scala 2.12.10 and 2.13.0, SBT 1.3.0, Cats 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,9 +6,9 @@ object Dependencies {
   val `kind-projector`       = "org.typelevel"               % "kind-projector"            % "0.10.3"
 
   object Cats {
-    private val version = "1.6.1"
+    private val version = "2.0.0"
     val core   = "org.typelevel" %% "cats-core"   % version
     val laws   = "org.typelevel" %% "cats-laws"   % version
-    val effect = "org.typelevel" %% "cats-effect" % "1.4.0"
+    val effect = "org.typelevel" %% "cats-effect" % "2.0.0"
   }
 }
