b2df9aae2bd81245c2fc866567be8aae40ae4d65
Scala Steward <me@scala-steward.org>
2020-02-21 14:25:09 +0100

    Update discipline-scalatest to 1.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val scalatest              = "org.scalatest"              %% "scalatest"                 % "3.1.0"
-  val discipline             = "org.typelevel"              %% "discipline-scalatest"      % "1.0.0-RC1"
+  val discipline             = "org.typelevel"              %% "discipline-scalatest"      % "1.0.1"
   val `scalacheck-shapeless` = "com.github.alexarchambault" %% "scalacheck-shapeless_1.14" % "1.2.3"
   val `kind-projector`       = "org.typelevel"               % "kind-projector"            % "0.10.3"
 
