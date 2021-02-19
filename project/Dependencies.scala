a7a6981f3334e9efe07a45512d775153996e4f66
Scala Steward <me@scala-steward.org>
2021-02-19 16:42:46 +0100

    Update scalatest to 3.2.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val scalatest              = "org.scalatest"              %% "scalatest"                 % "3.2.3"
+  val scalatest              = "org.scalatest"              %% "scalatest"                 % "3.2.5"
   val discipline             = "org.typelevel"              %% "discipline-scalatest"      % "2.1.0"
   val `scalacheck-shapeless` = "com.github.alexarchambault" %% "scalacheck-shapeless_1.14" % "1.2.5"
   val `kind-projector`       = "org.typelevel"               % "kind-projector"            % "0.11.2"
